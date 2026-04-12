c8ce5fccadbb28c8cd134548cfc0c3d225e1be72
Jose Hipolito <josehipolito@Joses-MacBook-Pro-6.local>
2026-04-11 21:28:50 -0500

    added an interpreter

--- src/Interpreter.java
+++ src/Interpreter.java
@@ -1,7 +1,31 @@
 package stac;
+import java.io.IOException;
+import stac.AST.*;
 
 public class Interpreter {
-    public static void main(String[] args) {
-        
-    }
+	public static void main(String[] args) {
+		if(args.length == 0) {
+			System.out.println("Usage: stac <filename>");
+			return;
+		}
+
+		Reader reader = new Reader();
+		Evaluator eval = new Evaluator();
+
+		Program p = null;
+		try {
+			p = reader.read(args[0]);
+			if(p == null || p.boyd() == null || p.body().isEmpty()){
+				System.out.println("Error: empty program.");
+				return;
+			}
+			eval.run(p);
+		}
+		catch (IOException e) {
+			System.out.println("Error reading file: " + e.getMessage());
+		}
+		catch (NullPointerException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+	}
 }
